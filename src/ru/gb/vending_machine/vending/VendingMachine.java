c39062793ca4023e7195630171e59be6aac30b12
Radmir <Rhody_17@mail.ru>
2024-07-30 20:07:28 +0300

    Урок 1. Принципы ООП: Инкапсуляция, наследование, полиморфизм

--- src/ru/gb/vending_machine/vending/VendingMachine.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.vending_machine.vending;
-
-import ru.gb.vending_machine.products.Product;
-
-public interface VendingMachine {
-    Product getProduct(String name, int volume, int temperature);
-}
